68d07c55854337b996515f00404027d800193b25
RisMouZ <palcailler@gmail.com>
2022-08-04 11:02:38 +0200

    update deploy script

--- truffle/migrations/1_Contract.js
+++ truffle/migrations/1_Contract.js
@@ -2,6 +2,7 @@ const Staking = artifacts.require("Staking");
 const annualRewardRate = 150;
 const cooldown = 10;
 const minimumReward = 0;
+const stakingTokenAddress = 0;
 
 module.exports = function (deployer) {
   deployer.deploy(Staking, annualRewardRate, cooldown, minimumReward);
